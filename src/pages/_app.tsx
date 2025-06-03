2da9a56ae4068648b787b3b65ad8b74151af1b05
APapeIsName <gms72901217@gmail.com>
2025-06-03 18:24:49 +0900

    글로벌 레이아웃 설정하기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import GlobalLayout from "@/components/global-layout";
+import { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
-
-  const onClickButton = () => {
-    router.push("/test");
-  }
 
   return (
     <>
-      <header>
-        <Link href={"/"}>홈</Link>
-        &nbsp;
-        <Link href={"/search"} prefetch={false}>search</Link>
-        &nbsp;
-        <Link href={"/book/1"}>book/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-        <div>글로벌 헤더</div>
-      </header>
-      <Component {...pageProps} />
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
     </>
   );
 }
