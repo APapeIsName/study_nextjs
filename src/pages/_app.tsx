c61653cc10bb04c9faf9cb9d2f26f2e64baa5389
APapeIsName <gms72901217@gmail.com>
2025-06-08 15:37:57 +0900

    페이지별 레이아웃 설정하기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import GlobalLayout from "@/components/global-layout";
+import { NextPage } from "next";
 import { AppProps } from "next/app";
-export default function App({ Component, pageProps }: AppProps) {
+import { ReactNode } from "react";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({ Component, pageProps }: AppProps & { Component: NextPageWithLayout }) {
+
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
   return (
     <>
       <GlobalLayout>
-        <Component {...pageProps} />
+        {getLayout(<Component {...pageProps} />)}
       </GlobalLayout>
     </>
   );
