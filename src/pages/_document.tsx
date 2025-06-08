b81cf8f6b69a7d5b497edd5abe64f04cca580f34
APapeIsName <gms72901217@gmail.com>
2025-06-08 15:45:41 +0900

    한입북스 UI 구현하기

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="ko">
+    <Html lang="kr">
       <Head />
       <body>
         <Main />
